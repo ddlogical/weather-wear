d4559ad6df08e78a3e1a91785a7c469f649088d1
2dl0gical <2dl0gical@gmail.com>
2025-01-19 11:20:35 +0200

    Location proxy fix

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 export default {
   MIN_HOT_TEMP: 10,
   API_USER_IP_URL: 'https://api.ipify.org?format=json',
-  API_USER_LOCATION_URL: `http://ip-api.com/json`,
+  API_USER_LOCATION_URL: 'api/location',
   API_CITIES_URL: '/api/cities',
   API_WEATHER_URL: '/api/weather'
 }
